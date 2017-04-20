1c7fd4c553b78d5000e4970ee86fc2f0f90f49c5
erna <noviasafitrierna@gmail.com>
2017-04-20 09:47:27 +0700

    uploadfolder

---
+++ uploadfolder.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post" enctype="multipart/form-data">
+    <input type="file" name="files[]" id="files" multiple="" directory="" webkitdirectory="" mozdirectory="">
+    <input class="button" type="submit" value="Upload" />
+</form>
+	<?php
+	$count = 0;
+	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    foreach ($_FILES['files']['name'] as $i => $name) {
+        if (strlen($_FILES['files']['name'][$i]) > 1) {
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], 'upload/'.$name)) {
+                $count++;
+            }
+        }
+    }
+}
+	  ?>
+</body>
+</html>
